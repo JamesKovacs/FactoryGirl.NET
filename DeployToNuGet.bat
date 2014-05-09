41a9f3e05f70c85179915dc67824f046e0af703b
James Kovacs <jkovacs@post.harvard.edu>
2014-05-09 10:51:15 -0600

    [Issue#9] Removed NuGet API Key and prompt for it instead

--- DeployToNuGet.bat
+++ DeployToNuGet.bat
@@ -2,6 +2,7 @@ nuget.exe update -self
 ECHO Y | DEL *.nupkg
 
 #NOTE - Using an account for factorygirl@meinershagen.net to publish to NuGet.org.  This can be changed in the future.
-nuget.exe setApiKey 57dc68f8-50fb-46fe-a99b-175c9099673e
+set /p NuGetApiKey= Please enter the project's NuGet API Key: 
+nuget.exe setApiKey %NuGetApiKey%
 nuget.exe pack FactoryGirl.NET\FactoryGirl.NET.csproj
 nuget.exe push *.nupkg
