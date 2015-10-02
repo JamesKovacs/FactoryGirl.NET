6cb3ea372731670ccef4192b4a0e045d9d0293a9
Alla Hoffman <alla.hoffman@buildium.com>
2015-10-02 14:33:00 -0400

    Library starts out with sequenced id factories registered

--- FactoryGirl.NET/FactoryGirl.cs
+++ FactoryGirl.NET/FactoryGirl.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace FactoryGirl.NET
 {
     public static class FactoryGirl {
-        private static readonly IDictionary<Type, Func<object>> builders = new Dictionary<Type, Func<object>>();
+        private static int m_counter;
+        private static readonly IDictionary<Type, Func<object>> builders = new Dictionary<Type, Func<object>>
+        {
+            //Sequenced Id factories are pre-registered
+            {typeof(int), () => Interlocked.Increment(ref m_counter)},
+            {typeof(uint), () => Interlocked.Increment(ref m_counter)}
+        };
  
         public static void Define<T>(Func<T> builder) {
             if(builders.ContainsKey(typeof(T))) throw new DuplicateFactoryException(typeof(T).Name + " is already registered. You can only register one factory per type.");
